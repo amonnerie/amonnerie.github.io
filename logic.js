aa797adfd3fbd33873be85d942e5114a70c10d7e
amonnerie <monnerie.andrea@gmail.com>
2023-07-29 17:19:06 -0700

    Project 3

--- logic.js
+++ logic.js
@@ -1,78 +1,309 @@
-const api_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+/* Javascript for project 3 */
+//constant/static values
+const data_json = "Shark_AttacksDB.Events.json";
+const geo_json = "Project3.cleanLocationDB.json";
+const dropdowns = document.querySelectorAll('select');
+const activityBins = ["Swimming", "Fishing", "Surfing", "Playing", "Floating", "Kayaking","Shark"];
+const typeBins = ["Unprovoked", "Provoked"];
+//const ctx = document.getElementById('visual2').getContext('2d');
 
-//get color function
-function getColor(d) {
-    return d > 90 ? '#d73027' :
-           d > 70  ? '#fc8d59' :
-           d > 50  ? '#fee08b' :
-           d > 30  ? '#d9ef8b' :
-           d > 10   ? '#91cf60' :
-                      '#1a9850';
-};
+//constant variables for map1//
+const marker_id = "1";
+var mapMarkers = [];
+const map = L.map("visual1", {
+  center: [40.752895, -101.010851],
+  zoom: 4
 
-function init() {
-    d3.json(api_url).then(function(json_data) {
-        let data = json_data.features;
+});
 
-        //console.log("data", data);
-
-        const map = L.map('map').setView([37.954585, -116.884660], 5);
-
-
-        //make the legend
-        var legend = L.control({position: 'bottomright'});
-
-        //add a tile layer, setting the style of map
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//*map with color
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
+//**************************//
 
-        //get the data on map from json
-        data.forEach(feature => {
-            const { geometry, properties } = feature;
-            const { coordinates } = geometry;
-            const [longitude, latitude, depth] = coordinates;
-      
-            // Extract earthquake properties for the pop up
-            const { mag, place } = properties;
-            
-            // Customize the marker appearance based on the earthquake magnitude
-            const marker = L.circleMarker([latitude, longitude], {
-              radius: mag * 5,
-              fillColor: getColor(depth),
-              color: 'white',
-              weight: 1,
-              opacity: 1,
-              fillOpacity: 0.7,
-            }).addTo(map);
-            
-            //add popup when user clicks on an event on map
-            marker.bindPopup(`longitude: ${longitude}
-                            <br>latitude: ${latitude}
-                            <br>Location: ${place}
-                            <br>Magnitude: ${mag}
-                            <br>Depth ${depth}
-                            <br>`);
+//create event listener in order to get 
+//all values from dropdowns
+dropdowns.forEach(dropdown => {
+  dropdown.addEventListener('change', () => {
+    const values = getValues();
+    console.log("current values:", values);
 
-            });
-
-            //create the static legend
-            legend.onAdd = function (map) {
-
-                var div = L.DomUtil.create('div', 'legend'),
-                    depths = [-10, 10, 30, 50, 70, 90],
-                    labels = [];
-            
-                for (var i = 0; i < depths.length; i++) {
-                    div.innerHTML +=
-                        '<i style="background:' + getColor(depths[i] + 1) + '"></i> ' +
-                        depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + '<br>' : '+');
-                        console.log(depths[i], getColor(depths[i] + 1));
-                        }
-                return div;
-                };
-                legend.addTo(map);
+    //making maps and graphs here upon updates
+    map1(values);
+    graph1(values);
+    graph2(values);
+  });
 });
+
+//the init function
+function init() {
+
+  const values = getValues();
+  console.log("current values:", values);
+  map1(values);
+  graph1(values);
+  graph2(values);
 };
 
+
+//***helpful functions
+function removeDepulicates(array) {
+  let output = Array.from(new Set(array))
+  return output
+}
+
+function getValues() {
+  var dropdownValues = {};
+
+  dropdowns.forEach(dropdown => {
+    //get the id of each dropdown (ex: "selyear")
+    const dropdownId = dropdown.id;
+
+    //get value of each dropdown
+    const selectedValue = dropdown.value;
+
+    //create dict
+    dropdownValues[dropdownId] = selectedValue;
+  });
+
+  return dropdownValues;
+};
+
+function map1(values) {
+
+  d3.json(geo_json).then((data) => {
+
+    let filteredData = filterData(values, data);
+
+    if (mapMarkers.length > 0) {
+      for(var i = 0; i < mapMarkers.length; i++){
+        map.removeLayer(mapMarkers[i]);
+      }
+    };
+
+    filteredData.forEach(feature => {
+    
+    var sharkIcon = L.icon({
+      iconUrl: 'https://static.thenounproject.com/png/1220701-200.png',
+  
+      iconSize:     [35, 35], // size of the icon
+      iconAnchor:   [22, 34], // point of the icon which will correspond to marker's location
+      popupAnchor:  [-1, -20] // point from which the popup should open relative to the iconAnchor
+  });
+    var marker = L.marker([feature.Lat, feature.Lng],
+    {icon: sharkIcon}).addTo(map);
+    this.mapMarkers.push(marker);
+
+    marker.bindPopup(`<b>Date:</b> ${feature.Date}
+      <br><b>Location:</b> ${feature.Location}
+      <br><b>Country:</b> ${feature.Country}
+      <br><b>Type of Attack:</b> ${feature.Type}
+      <br><b>Activity When Attacked:</b> ${feature.Activity}
+      <br><b>Injury:</b> ${feature.Injury}
+      <br><b>Fatal:</b> ${feature.Fatal}
+      <br><b>More Info:</b> <a href=${feature.Link} target="_blank">PDF</a>
+      <br>`);
+});
+
+  if(filteredData.length == 0) {
+    alert("No known data with current parameters");
+  }
+  });
+  };
+
+function graph1(values) {
+
+  d3.json(geo_json).then((data) => {
+  let filteredData = filterData(values, data);
+  let dates = [];
+  for (i=0; i < filteredData.length; i++) {
+    dates.push(filteredData[i].Date);
+  };
+  dates.sort();
+
+  var trace = {
+    x: dates,
+    type: 'histogram',
+  };
+  var layout = {
+    title: {
+      text:'Frequency of Attacks by Date',
+      y: 0.95
+    },
+    xaxis: {
+      title: {
+        text: 'Date of Attack',
+        y: 1
+      },
+    },
+    yaxis: {
+      title: {
+        text: 'Frequency',
+      },
+      tickmode: "array",
+      tickvals: [0, 1, 2, 3, 4],
+    },
+    margin: {
+      t: 30,
+      b: 100
+    },
+    pad: {
+      b: 20
+    },
+  };
+
+  var dataTrace = [trace];
+  Plotly.newPlot('visual3', dataTrace, layout);
+  });
+}
+
+function graph2(values) {
+  d3.json(geo_json).then((data) => {
+    let filteredData = filterData(values, data);
+
+    var tableSpot = $("#visual2")
+    
+    if ($.fn.dataTable.isDataTable('#visual2')) {
+      tableSpot.DataTable().destroy();
+    };
+
+    tableSpot.DataTable ({
+      "data" : filteredData,
+      "columns" :  [
+        {
+            "data": "Date",
+            "title": "Date",
+            "defaultContent": "<i>NA</i>"
+        },
+        {
+          "data": "Location",
+          "title": "Location",
+          "defaultContent": "<i>NA</i>"
+        },
+        {
+          "data": "Type",
+          "title": "Type of Attack",
+          "defaultContent": "<i>NA</i>"
+        },
+        {
+          "data": "Activity",
+          "title": "Activity",
+          "defaultContent": "<i>Unknown</i>"
+        }],
+      scrollResize: true,
+      scrollY: 100,
+      scrollCollapse: true,
+      paging: false,
+      searching: false
+      
+  });
+  });
+};
+
+function filterData(values, data) {
+  let newData = data;
+  
+  if(values["selyear"] != "All") {
+    newData = data.filter(filterYear);
+  }
+  if(values["selcountry"] != "All") {
+    newData = newData.filter(filterCountry);
+  }
+  if(values["seltype"] != "All") {
+    newData = gatherType(values, newData);
+  }
+  if(values["selact"] != "All") {
+    newData = gatherAct(values, newData);
+  }
+  if(values["selfatal"] != "All") {
+    newData = newData.filter(filterFatal);
+  }
+
+  return newData
+};
+
+function filterYear(events) {
+  values = getValues();
+  return events.Year == values["selyear"];
+};
+
+function filterCountry(events) {
+  values = getValues();
+  return events.Country == values["selcountry"];
+};
+
+function filterFatal(events){
+  values = getValues();
+  return events.Fatal == values["selfatal"];
+};
+
+function gatherType(values, data){
+  selType = values["seltype"];
+  let filteredType = [];
+
+  for(i=0; i<data.length; i++){
+    type = data[i].Type;
+
+    if(selType === "Other"){
+      
+      for (let j = 0; j < typeBins.length; j++){
+        if (type.includes(typeBins[j])){
+          break;
+        } else if (j = 1) {
+          filteredType.push(data[i]);
+        } else {
+          break;
+        };
+      };
+    } else {
+      if(type.includes(selType)) {
+        filteredType.push(data[i]);
+      };
+    };
+  };
+  return filteredType;
+};
+
+
+function gatherAct(values, data) {
+  const selAct = values["selact"].replace(" Related Activites", "");
+  let filteredAct = [];
+  
+  for (let i = 0; i < data.length; i++){
+    eventact = data[i].Activity;
+    
+    if(selAct === "Unknown") {
+      //find the activities that are undefined
+      if(eventact === undefined){
+        filteredAct.push(data[i]);
+      }
+      } else if (selAct != "Other") {
+      //find activites by selact
+        if(eventact === undefined){
+        } else {
+          if(eventact.includes(selAct.toLowerCase()) || eventact.includes(selAct)){
+            filteredAct.push(data[i]);
+          }
+        }
+        
+      } else {
+        //selAct is "Other"
+        for (let j = 0; j < activityBins.length; j++){
+          if(eventact === undefined || eventact === null){
+            break;
+            //leave this for loop to get new eventact
+          } else if (eventact.includes(activityBins[j].toLowerCase()) || eventact.includes(activityBins[j])){
+            break;
+            //leave this for loop to get new eventact
+          } else if (j == activityBins.length-1) {
+            filteredAct.push(data[i]);
+          };
+        };
+      };
+
+    };
+//return the filtered Data
+return filteredAct;
+};
 init();
